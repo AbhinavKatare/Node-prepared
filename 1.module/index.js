00617d6233a7fd2ee028aedbf96f0fcbb2fcb238
Abhinav Katare <therockstarstar695@gmail.com>
2026-06-04 20:40:08 +0530

    Update console log message in index.js

--- 1.module/index.js
+++ 1.module/index.js
@@ -1,1 +1,2 @@
-console.log ("this is index.js");
+console.log ("this is Abhinav Katare in this index.js");
+// we are strarting my personal favorite series of node code push which is from zero to advanced industry type deep tech for perfect backend if you want you can continue with my this repo
