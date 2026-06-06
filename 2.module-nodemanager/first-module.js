7b9bb5c6c2f5339418d524db569574cc3ae395dd
AbhinavKatare <abhinavkatare18@gmail.com>
2026-06-06 17:20:40 +0530

    now nodemanager

---
+++ 2.module-nodemanager/first-module.js
@@ -0,0 +1,9 @@
+// require and module.exports are the two main things that we will be using in this module
+
+// require is used to import other modules, and module.exports is used to export the functionality of this module
+
+function add (a,b){
+    return a + b 
+}; 
+
+module.exports = {add} 
